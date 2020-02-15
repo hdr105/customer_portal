1f6bb8c928638e7d4cadbd94444484194d9b98c9
hdr105 <haadi.javaid@gmail.com>
2020-02-15 16:20:50 +0000

    Change chat script

--- resources/views/layouts/partials/js.blade.php
+++ resources/views/layouts/partials/js.blade.php
@@ -76,16 +76,18 @@ $("#end_date").change(function () {
 });
 
 </script>
+<!--Start of Tawk.to Script-->
 <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/5dcb26dad96992700fc721f2/default';
+s1.src='https://embed.tawk.to/58a34de269c2661545bfe1eb/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
+<!--End of Tawk.to Script-->
 
 @yield('additionalJS')
